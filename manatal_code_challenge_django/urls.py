2c5a34b7e0b14d17f381fdb17def90d7ced6b569
Poompatai Puntitpong <poomtapai@hotmail.com>
2020-02-10 21:59:14 +0700

    Complete codebase for step3

--- manatal_code_challenge_django/urls.py
+++ manatal_code_challenge_django/urls.py
@@ -16,18 +16,20 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from pages.models import School, Student
-from rest_framework import routers
+from rest_framework_nested import routers
 
-from pages.views import UserViewSet, SchoolViewSet, StudentViewSet
+from pages.views import SchoolViewSet, StudentViewSet
 
 # Routers provide an easy way of automatically determining the URL conf.
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
+router = routers.SimpleRouter()
 router.register(r'schools', SchoolViewSet)
-router.register(r'students', StudentViewSet)
+router.register(r'students',StudentViewSet, basename='Student')
+
+domains_router = routers.NestedSimpleRouter(router, r'schools', lookup='school')
+domains_router.register(r'students', StudentViewSet, basename='Student')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path(r'', include(router.urls)),
-    path(r'', include('rest_framework.urls', namespace='rest_framework'))
+    path(r'', include(domains_router.urls))
 ]
--- pages/views.py
+++ pages/views.py
@@ -14,16 +14,25 @@ class DestroyWithPayloadMixing(object):
         return Response(serializer.data, status=status.HTTP_200_OK)
 
 # ViewSets define the view behavior.
-class UserViewSet(DestroyWithPayloadMixing, viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-# ViewSets define the view behavior.
 class SchoolViewSet(DestroyWithPayloadMixing, viewsets.ModelViewSet):
     queryset = School.objects.all()
     serializer_class = SchoolSerializer
 
 # ViewSets define the view behavior.
 class StudentViewSet(DestroyWithPayloadMixing, viewsets.ModelViewSet):
-    queryset = Student.objects.all()
     serializer_class = StudentSerializer
+    def get_queryset(self):
+        if 'school_pk' in self.kwargs:
+            return Student.objects.filter(school_id=self.kwargs['school_pk'])
+        else:
+            return Student.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        """If is called inside school, we use data from the url(ignore passed data) else just use from the passed data"""
+        if 'school_pk' in self.kwargs:
+            request.data['school'] = self.kwargs['school_pk']
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
