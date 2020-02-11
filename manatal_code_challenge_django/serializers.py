beb1ce66654dc9ee9afe73206aef1b0297864213
Poompatai Puntitpong <poomtapai@hotmail.com>
2020-02-11 09:13:42 +0700

    move school validation to field level

--- manatal_code_challenge_django/serializers.py
+++ manatal_code_challenge_django/serializers.py
@@ -21,22 +21,20 @@ class StudentSerializer(serializers.ModelSerializer):
     def schoolFullError(self, school):
         raise serializers.ValidationError("School " + school.name + " already has maximum number of students")
 
-    def validate(self, data):
+    def validate_school(self, data):
         """check if school is full"""
         method = self.context['request'].method
-        if 'school' in data:
-            school = data['school']
-            students_count = Student.objects.filter(school=school.id).count()
+        school = data
+        students_count = Student.objects.filter(school=school.id).count()
 
-            """2 cases 1.creating
-                       2.update with school switching"""
+        """2 cases 1.creating
+                   2.update with school switching"""
+        if method == 'POST' and students_count >= school.max_student:
+            self.schoolFullError(school)
 
-            if method == 'POST' and students_count >= school.max_student:
-                self.schoolFullError(school)
-
-            elif ((method == 'PUT' or method == 'PATCH')
-                 and school.id != self.instance.school.id
-                 and students_count >= school.max_student):
-                self.schoolFullError(school)
+        elif ((method == 'PUT' or method == 'PATCH')
+             and school.id != self.instance.school.id
+             and students_count >= school.max_student):
+            self.schoolFullError(school)
 
         return data
