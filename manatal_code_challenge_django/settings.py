3cc898a4adc37f410ddf524953b910d6e45f0b6b
Poompatai Puntitpong <poomtapai@hotmail.com>
2020-02-12 22:42:44 +0700

    Make url end with slash optional

--- manatal_code_challenge_django/settings.py
+++ manatal_code_challenge_django/settings.py
@@ -123,7 +123,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-APPEND_SLASH = True
+APPEND_SLASH = False
 
 
 # Static files (CSS, JavaScript, Images)
--- manatal_code_challenge_django/urls.py
+++ manatal_code_challenge_django/urls.py
@@ -6,15 +6,14 @@ from rest_framework_nested import routers
 from pages.views import SchoolViewSet, StudentViewSet
 
 # Routers provide an easy way of automatically determining the URL conf.
-router = routers.SimpleRouter()
-router.register(r'schools', SchoolViewSet)
-router.register(r'students',StudentViewSet, basename='Student')
+router = routers.SimpleRouter(trailing_slash=False)
+router.register(r'schools/?', SchoolViewSet)
+router.register(r'students/?',StudentViewSet, basename='Student')
 
-domains_router = routers.NestedSimpleRouter(router, r'schools', lookup='school')
-domains_router.register(r'students', StudentViewSet, basename='Student')
+domains_router = routers.NestedSimpleRouter(router, r'schools/?', lookup='school')
+domains_router.register(r'students/?', StudentViewSet, basename='Student')
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path(r'', include(router.urls)),
     path(r'', include(domains_router.urls))
 ]
