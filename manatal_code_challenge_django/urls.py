d677e7b25f4023c069ab4ba7c03eea5add776f2d
Poompatai Puntitpong <poomtapai@hotmail.com>
2020-02-12 23:32:26 +0700

    Apply routing to both slash and non-slash url, add tests to prove it works

--- manatal_code_challenge_django/urls.py
+++ manatal_code_challenge_django/urls.py
@@ -5,15 +5,20 @@ from rest_framework_nested import routers
 
 from pages.views import SchoolViewSet, StudentViewSet
 
-# Routers provide an easy way of automatically determining the URL conf.
-router = routers.SimpleRouter(trailing_slash=False)
-router.register(r'schools/?', SchoolViewSet)
-router.register(r'students/?',StudentViewSet, basename='Student')
+def optional_trailing_slash(urls):
+   for url in urls[0]:
+       url.pattern._regex = url.pattern._regex.replace('/$', '/?$')
+   return urls
 
-domains_router = routers.NestedSimpleRouter(router, r'schools/?', lookup='school')
-domains_router.register(r'students/?', StudentViewSet, basename='Student')
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.SimpleRouter()
+router.register(r'schools', SchoolViewSet)
+router.register(r'students',StudentViewSet, basename='Student')
+
+domains_router = routers.NestedSimpleRouter(router, r'schools', lookup='school')
+domains_router.register(r'students', StudentViewSet, basename='Student')
 
 urlpatterns = [
-    path(r'', include(router.urls)),
-    path(r'', include(domains_router.urls))
+    path(r'', optional_trailing_slash(include(router.urls))),
+    path(r'', optional_trailing_slash(include(domains_router.urls)))
 ]
