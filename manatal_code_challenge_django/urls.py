dc9d52b120e93c559a6f4782aaa39cf2f9b90c14
Poompatai Puntitpong <poomtapai@hotmail.com>
2021-04-10 13:10:38 +0700

    settings

--- manatal_code_challenge_django/urls.py
+++ manatal_code_challenge_django/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from pages.models import School, Student
 from rest_framework_nested import routers
+from django.conf import settings
 
 from pages.views import SchoolViewSet, StudentViewSet, homePageView
 
